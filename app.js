e783281322801e12ba3703a31991ba4f9ea16766
mohsen104 <m.karimvand.84@gmail.com>
2024-12-11 23:17:54 +0330

    fix sameSite

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === 'production' ? "strict" : "lax",
         maxAge: 1000 * 60 * 60 * 1, // 1 hour
     },
 }));
